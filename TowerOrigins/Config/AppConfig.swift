f4f378b4b2c5fc7562cc2e503b2b5edbe754b6df
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-16 21:15:20 +0300

    Update AppConfig.swift

--- TowerOrigins/Config/AppConfig.swift
+++ TowerOrigins/Config/AppConfig.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 enum AppConfig {
-    static let originLink = URL(string: "https://keitaro-zaglushka.com")!
-    static let privacyPolicyURL = URL(string: "https://hallowtommy.github.io/towerorigins-privacy")!
-    static let supportEmail = "support@towerorigins.app"
+    static let originLink = URL(string: "https://obrintal.com/txZ7KR")!
+    static let privacyPolicyURL = URL(string: "https://www.termsfeed.com/live/6a2469c1-5c57-41bb-b641-dbe34817d232")!
+    static let supportEmail = "nov1kovva@icloud.com"
 
     static var versionLine: String {
         let mv = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
